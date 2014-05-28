b2960959f27ead5f09015478b004947795a2f291
chaca <dgraselli@gmail.com>
2014-05-28 12:51:39 -0300

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,13 @@ FirstApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
 
-
+config.action_mailer.default_url_options = { :host => 'adgw.herokuapp.com' }
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
 end
