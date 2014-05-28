e43f668ce33635546e2d8e58c6ac8ae7d1792cfc
chaca <dgraselli@gmail.com>
2014-05-28 12:49:30 -0300

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,5 @@ FirstApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
 
-    config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    authentication: "plain",
-    user_name: "app25549696@heroku.com",
-    password: ENV['SMTP_PASSWORD'],
-    enable_starttls_auto: false
-  }
-  config.action_mailer.raise_delivery_errors = true
+
 end
