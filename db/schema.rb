60fa3314229e235a55205d81bcbf57a9eaefe70f
chaca <dgraselli@gmail.com>
2014-06-07 16:30:44 -0300

    task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607192821) do
+ActiveRecord::Schema.define(version: 20140607193027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20140607192821) do
     t.float    "lectura_consumo"
     t.float    "accuracy"
     t.string   "pos"
+    t.string   "device"
   end
 
   add_index "lecturas", ["lecturista_id"], name: "index_lecturas_on_lecturista_id", using: :btree
